b27a5337e493b60521286e85a6e829e73f3aff75
mantasha-shaikh <mantasha2513604@gmail.com>
2025-11-18 15:01:23 +0530

    adding Page Oreder & tracking ..

--- Ecommerce-Projects/src/App.jsx
+++ Ecommerce-Projects/src/App.jsx
@@ -1,5 +1,7 @@
 import { HomePage } from "./pages/HomePages";
 import { CheckoutPage } from "./pages/CheckoutPage";
+import { OrdersPage } from "./pages/OrderPage";
+import { TrackingPage } from "./pages/TrackingPage";
 import { Routes, Route } from "react-router";
 import "./App.css";
 
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route index element={<HomePage />}/>
         <Route path="checkout" element={<CheckoutPage/>} />
+         <Route path="orders" element={<OrdersPage/>} />
+         <Route path="tracking" element={<TrackingPage/>} />
       </Routes>
     </>
   );
